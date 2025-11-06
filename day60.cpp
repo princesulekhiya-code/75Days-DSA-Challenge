6ae4e9720b4fb657a3a9c2c9ecf44278a379519c
princesulekhiya-code <146360988+princesulekhiya-code@users.noreply.github.com>
2025-11-06 23:07:09 +0530

    Implement countNodes function for binary tree

---
+++ day60.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (!root) return 0;
+        int lh = heightLeft(root), rh = heightRight(root);
+        if (lh == rh) return (1 << lh) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+private:
+    int heightLeft(TreeNode* node) {
+        int h = 0;
+        while (node) {
+            h++;
+            node = node->left;
+        }
+        return h;
+    }
+    int heightRight(TreeNode* node) {
+        int h = 0;
+        while (node) {
+            h++;
+            node = node->right;
+        }
+        return h;
+    }
+};
