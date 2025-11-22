b32905757281d5a4a5d104549430b88e7ad62e85
princesulekhiya-code <146360988+princesulekhiya-code@users.noreply.github.com>
2025-11-23 00:01:58 +0530

    Implement string reorganizing algorithm

---
+++ 74.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        vector<int> freq(26, 0);
+        for (char c : s) freq[c - 'a']++;
+
+        priority_queue<pair<int, char>> pq;
+        for (int i = 0; i < 26; i++)
+            if (freq[i] > 0)
+                pq.push({freq[i], char('a' + i)});
+
+        string result = "";
+        pair<int, char> prev = {0, '#'};
+
+        while (!pq.empty()) {
+            auto curr = pq.top();
+            pq.pop();
+            result += curr.second;
+            curr.first--;
+
+            if (prev.first > 0)
+                pq.push(prev);
+
+            prev = curr;
+        }
+
+        if (result.size() != s.size()) return "";
+        return result;
+    }
+};
